4095c29316655190350f9818e9711f6802965bf5
Moo-ananchai <ananchai.pha@gmail.com>
2021-02-21 21:05:44 +0700

    init commit

---
+++ App.js
@@ -0,0 +1,97 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import Geolocation from '@react-native-community/geolocation';
+
+const App: () => React$Node = () => {
+  const [lat, setLat] = useState(0)
+  const [long, setLong] = useState(0)
+  Geolocation.getCurrentPosition(info => {
+    setLat(info.coords.latitude)
+    setLong(info.coords.longitude)
+  }
+  );
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <View style={styles.body}>
+          <Text style={styles.sectionTitle}>Current Location</Text>
+
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>lat: {lat}</Text>
+              <Text style={styles.sectionTitle}>long: {long}</Text>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
